0b1126b7347d9ebe0c0b463b249fd6b988983cd8
Ameya Deokule <deokuleameya@gmail.com>
2021-07-13 22:36:09 +0530

    graph added

---
+++ src/components/Graph/index.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ReactApexCharts from 'react-apexcharts'
+import './Graph.css'
+
+function Graph({ statesData }) {
+	const seriesData = statesData.map((state) => state.cases)
+	console.log(seriesData)
+	const seriesCategories = statesData.map((state) => state.state)
+
+	const series = [
+		{
+			name: 'Total Cases',
+			data: seriesData,
+		},
+	]
+
+	const options = {
+		colors: ['#28C76F'],
+		chart: {
+			height: 400,
+			zoom: {
+				enabled: true,
+				type: 'xy',
+			},
+			toolbar: {
+				show: true,
+			},
+		},
+
+		xaxis: {
+			type: 'category',
+			categories: seriesCategories,
+			labels: {
+				style: {
+					fontSize: '10px',
+				},
+			},
+			tickAmount: 7,
+			tickPlacement: 'between',
+		},
+		yaxis: {
+			tickAmount: 7,
+		},
+		markers: {
+			strokeWidth: 0,
+		},
+		legend: {
+			show: false,
+		},
+	}
+
+	return (
+		<ReactApexCharts
+			className='map'
+			options={options}
+			type='scatter'
+			series={series}
+			height={400}
+		/>
+	)
+}
+
+export default Graph
